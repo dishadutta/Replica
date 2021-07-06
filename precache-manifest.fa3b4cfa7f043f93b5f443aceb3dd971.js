6f8da790913b21cb6a81a3be6d5a3b0bd80a9963
dishadutta <duttadisha12345@gmail.com>
2021-07-06 14:32:17 +0530

    Updates

---
+++ precache-manifest.fa3b4cfa7f043f93b5f443aceb3dd971.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edc6befa35cbce805708862978c0b5c2",
+    "url": "/Replica/index.html"
+  },
+  {
+    "revision": "e85043f71c74d4c3a14b",
+    "url": "/Replica/static/css/main.b9ad240e.chunk.css"
+  },
+  {
+    "revision": "152304cc45502ee33033",
+    "url": "/Replica/static/js/2.ce904cfd.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Replica/static/js/2.ce904cfd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e85043f71c74d4c3a14b",
+    "url": "/Replica/static/js/main.356f8fe0.chunk.js"
+  },
+  {
+    "revision": "4ee90ec9b5d945c03a83",
+    "url": "/Replica/static/js/runtime-main.179024a2.js"
+  },
+  {
+    "revision": "024804caf65d66daf14dbf23f8ddc601",
+    "url": "/Replica/static/media/call.024804ca.PNG"
+  },
+  {
+    "revision": "6751a2a10a2c1b41d3d261e7f652894f",
+    "url": "/Replica/static/media/chat.6751a2a1.PNG"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Replica/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a7227ec626fd2c68d0b34f9ddc0f626e",
+    "url": "/Replica/static/media/sticker2.a7227ec6.PNG"
+  },
+  {
+    "revision": "f3ffae56d8d0b9ba60191ef49ce8dd1a",
+    "url": "/Replica/static/media/stickers.f3ffae56.PNG"
+  }
+]);
